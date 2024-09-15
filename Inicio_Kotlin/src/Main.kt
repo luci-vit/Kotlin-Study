8763f4f1751e0cb77d4f08dde5c64103306cc5c9
luci-vit <lu.ci.an.nascimento20x@gmail.com>
2024-09-15 18:18:04 -0300

    Integrando pasta

---
+++ Inicio_Kotlin/src/Main.kt
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    //Forma de declarar variável sem especificando o tipo:
+    var nome = "Lucian"
+    var idade = 19
+
+    //Forma de declarar variável especificando o tipo:
+    var cpf: String = "866.469.925-92"
+
+    //Variáveis mutáveis
+    var salario = 5000.50f
+    salario = 6030.59f
+
+    //Variáveis imutáveis 
+    //val cpf: String = "866.469.925-92"
+
+
+}
